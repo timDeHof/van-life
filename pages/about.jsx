cc1274c9364801fddd8ee5459b902bb02bc5e329
timDeHof <this.is.tim.dehof@gmail.com>
2023-04-10 15:54:32 -0400

    moved images to public directory

--- pages/about.jsx
+++ pages/about.jsx
@@ -56,7 +56,7 @@ const SharedButton = styled.button`
 export default function About() {
   return (
     <AboutSection>
-      <HeaderIMG src='../public/images/camping.png' />
+      <HeaderIMG src='/images/camping.png' />
       <ContextWrapper>
         <Title>Don’t squeeze in a sedan when you could relax in a van.</Title>
         <Body>
