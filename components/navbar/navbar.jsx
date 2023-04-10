cc24888436d04d218137596ba59ae782a8a40b91
timDeHof <this.is.tim.dehof@gmail.com>
2023-04-10 14:50:03 -0400

    Refactor code to improve navigation styling and simplify logic in Home, About, and index.jsx pages

--- components/navbar/navbar.jsx
+++ components/navbar/navbar.jsx
@@ -1,52 +1,46 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
-import styled from 'styled-components';
+import React from "react"
+import { Link } from "react-router-dom"
+import styled from "styled-components"
 
 const NavbarWrapper = styled.ul`
-    background-color: #FFF7ED;
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    margin: 0;
-    padding: 24px 36px;
-    list-style-type: none;
+  background-color: #fff7ed;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  margin: 0;
+  padding: 24px 36px;
+  list-style-type: none;
 `
 const LinkWrapper = styled.div`
-    display: flex;
-    gap: 12px;
+  display: flex;
+  gap: 12px;
 `
 const NavbarLink = styled(Link)`
-    color: #4D4D4D;
-    font-family: 'Inter',sans-serif;
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 23px;
-    text-decoration: none;
-    text-align: right;
+  text-decoration: none;
+  color: #4d4d4d;
+  font-weight: 600;
+  padding: 5px 20px;
+  &:hover {
+    color: #161616;
+    text-decoration: underline;
+  }
 `
 const LogoLink = styled(NavbarLink)`
-    color: #000;
-    font-weight: 900;
-    font-size: 25.3531px;
-    line-height: 40px;
-    text-decoration: none;
-    text-transform: uppercase;
-    
-    & img {
-        width:144px;
-        height: auto;
-    } 
+  color: #000;
+  font-weight: 900;
+  font-size: 25px;
+  text-decoration: none;
+  text-transform: uppercase;
 `
 
 export function Navbar() {
-    return (
-        <NavbarWrapper>
-          <LogoLink to="/" ><img src="../../assets/logog.png"/></LogoLink>
-          <LinkWrapper>
-            <NavbarLink to="/about">About</NavbarLink>
-            <NavbarLink to="/vans">Vans</NavbarLink>
-          </LinkWrapper>
-        </NavbarWrapper>
-    )
+  return (
+    <NavbarWrapper>
+      <LogoLink to='/'>#VanLife</LogoLink>
+      <LinkWrapper>
+        <NavbarLink to='/about'>About</NavbarLink>
+        <NavbarLink to='/vans'>Vans</NavbarLink>
+      </LinkWrapper>
+    </NavbarWrapper>
+  )
 }
--- pages/home.jsx
+++ pages/home.jsx
@@ -1,67 +1,66 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from "react"
+import styled from "styled-components"
 
 const HomeWrapper = styled.div`
-    background: linear-gradient(0deg, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.46)), url("../../assets/mountain-view.png") no-repeat fixed center;
-    background-size: 100% 100%;
-    margin: 0;
-    padding: 0;
-    height: auto;
+  background: linear-gradient(0deg, rgba(0, 0, 0, 0.46), rgba(0, 0, 0, 0.46)),
+    url("../../assets/mountain-view.png") no-repeat center center;
+  -webkit-background-size: cover;
+  -moz-background-size: cover;
+  -o-background-size: cover;
+  background-size: cover;
+  min-height: 350px;
+  color: #fff;
+  padding: 45px 23px;
 `
 const CTA = styled.div`
-   display: flex;
-   flex-direction: column;
-   justify-content: center;
-   align-items: center;
-   color: #FFF;
-   padding: 65px 26px;
-    gap: 24px;
-   
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  color: #fff;
+  padding: 65px 26px;
+  gap: 24px;
 `
 const Title = styled.p`
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 800;
-    font-size: 36px;
-    line-height: 42px; 
-    margin: 0; 
+  font-weight: 700;
+  font-size: 2.25rem;
+  line-height: 42px;
 `
 
 const SubTitle = styled.p`
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 500;
-    font-size: 16px;
-    line-height: 24px;
-    margin: 0;
+  line-height: 24px;
 `
 
 const CTAButton = styled.button`
-    background: #FF8C38;
-    border-radius: 5px;
-    border: none;
-    margin: 0 26px;
-    max-width: 494px;
-    padding: 8px 143px;
-    
-    font-family: 'Inter';
-    font-style: normal;
-    font-weight: 600;
-    font-size: 16px;
-    line-spacing: 1.4;
-    line-height: 32px;
-    text-align: center;
-    color: #FFF;
+  display: inline-block;
+  text-align: center;
+  text-decoration: none;
+  background: #ff8c38;
+  border: none;
+  width: 100%;
+  margin-top: 27px;
+  padding-block: 0.75rem;
+  color: #fff;
+  font-weight: 700;
+  border-radius: 5px;
+  cursor: pointer;
+  transition: transform 0.1s ease-in-out;
+  &:hover {
+    transform: translate(1px, 1px);
+  }
 `
 
 export default function Home() {
   return (
-      <HomeWrapper>
-        <CTA>
-            <Title>You got the travel plans, we got the travel vans.</Title>
-            <SubTitle>Add adventure to your life by joining the #vanlife movement. Rent the perfect van to make your perfect road trip.</SubTitle>
-            <CTAButton>Find your van</CTAButton>
-        </CTA>
-      </HomeWrapper>
+    <HomeWrapper>
+      <CTA>
+        <Title>You got the travel plans, we got the travel vans.</Title>
+        <SubTitle>
+          Add adventure to your life by joining the #vanlife movement. Rent the
+          perfect van to make your perfect road trip.
+        </SubTitle>
+        <CTAButton>Find your van</CTAButton>
+      </CTA>
+    </HomeWrapper>
   )
 }
